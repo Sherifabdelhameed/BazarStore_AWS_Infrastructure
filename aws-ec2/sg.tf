3de3c4dc7071fd189083f9f02eb843ac422d51a2
Sherif Abdelhameed <sherifhameed30@gmail.com>
2025-05-05 19:22:40 +0300

    added ec2-jenkins server sg rules

--- aws-ec2/sg.tf
+++ aws-ec2/sg.tf
@@ -1,24 +1,41 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow SSH, HTTP, and HTTPS"
-  description = "Allow TLS inbound SSH, HTTP, and HTTPS and all outbound traffic"
+resource "aws_security_group" "security_group" {
+  name        = "ec2_security_group"
+  description = "Security group for Jenkins EC2 instance"
   vpc_id      = var.vpc_id
-  
 
   tags = {
-    Name = "allow_tls"
+    Name = "EC2-Jenkins-SG"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
+# Inbound rules
+resource "aws_security_group_rule" "allow_ssh" {
+  security_group_id = aws_security_group.security_group.id
+  type              = "ingress"
   from_port         = 22
-  ip_protocol       = "tcp"
   to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]  # Consider restricting this to your IP
+  description       = "Allow SSH from anywhere"
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+resource "aws_security_group_rule" "allow_jenkins" {
+  security_group_id = aws_security_group.security_group.id
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]  # Consider restricting this
+  description       = "Allow Jenkins web access"
+}
+
+# Outbound rule - allow all traffic
+resource "aws_security_group_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.security_group.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow all outbound traffic"
 }
