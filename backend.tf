38ff640488ecb4dfe1c1e3ade80ddb7e567eb9fe
Sherif Abdelhameed <sherifhameed30@gmail.com>
2025-05-05 12:44:35 +0300

    configured backend final

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "depiproject-tfstate "
-    key    = "path/to/my/key"
+    bucket = "depiproject-tfstate"
+    key    = "terraform.tfstate"
     region = "eu-north-1"
   }
 }
